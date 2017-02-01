20de1b9f98c22d722171292062fef599a5d85ff0
Kyle <kylejking@gmail.com>
2017-01-31 21:16:22 -0500

    Adding menus

--- teacher/teacher.ino
+++ teacher/teacher.ino
@@ -5,27 +5,48 @@
 SoftwareSerial XBee(2, 3);
 LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
 
-bool READY = true;
 byte ON_MASK = B10000000;
 byte OFF_MASK = B01000000;
+
+enum States {
+  Reading, Waiting, Judging
+};
+
+States STATE = Reading;
+
+void reading() {
+  lcd.clear();
+  lcd.print("-IT105 Jeopardy-");
+  lcd.setCursor(0,1);
+  lcd.print("<Test     Start>");
+
+  STATE = Reading;
+}
+
+void judging(char buzzer) {
+  lcd.clear();
+  lcd.setCursor(4,0);
+  lcd.print(buzzer);
+  lcd.print(" BUZZED");
+  lcd.setCursor(0,1);
+  lcd.print("<Wrong    Right>");
+  
+  STATE = Judging;
+}
 
 void setup() {  
   XBee.begin(9600);
   lcd.begin(16, 2);
-  lcd.print("-IT105 Jeopardy-");
-  lcd.setCursor(0,1);
+  reading();
 }
 
 void loop() {
   if (XBee.available()) {
     byte buzzer = XBee.read();
     
-    if (READY) {
+    if (STATE == Reading) {
       XBee.write(buzzer | ON_MASK);
-      
-      lcd.setCursor(4,1);
-      lcd.write(buzzer + '0');
-      lcd.print(" BUZZED");      
+      judging(buzzer + '0');
     }
   }
 }
